88042e62863bb61e03fb4b5e6d8f260e69690e70
blackbird <amermohammed928@gmail.com>
2020-05-28 18:22:55 +0200

    fix path isusse

--- bc_awareness/util.py
+++ bc_awareness/util.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 class Util:
     addons_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
     def __init__(self):
-        self.addons_path = self.addons_path.replace('bc_awareness', '')
+        self.addons_path = self.addons_path.replace('bc_awareness/bc_awareness', 'bc_awareness')
 
     def path(self, *paths):
         ''' Make a path
